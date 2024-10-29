ca1a93ff4fdda0b78abb2f16cc031c48e88b3881
altairBASIC <ignacio.ramirez.m@usach.cl>
2024-10-29 00:30:49 -0300

    Update milc.py

--- milc.py
+++ milc.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # pylint: disable=invalid-name, undefined-variable, used-before-assignment
 # pylama: ignore=E0602
 
@@ -22,17 +23,20 @@ Stage0 += shell(commands=[
      cd .. && rm -rf cmake-3.18.0 cmake-3.18.0.tar.gz'
 ])
 
+# Definición de comandos para QUDA
+quda_commands = '''
+    mkdir -p /var/tmp && cd /var/tmp && git clone --depth=1 --branch develop https://github.com/lattice/quda.git quda && cd - && 
+    cd /var/tmp/quda && 
+    mkdir -p /usr/local/quda && 
+    mkdir -p /var/tmp/quda/build && cd /var/tmp/quda/build && 
+    cmake -DCMAKE_INSTALL_PREFIX=/usr/local/quda -D CMAKE_BUILD_TYPE=RELEASE -D QUDA_DIRAC_CLOVER=ON -D QUDA_DIRAC_DOMAIN_WALL=ON -D QUDA_DIRAC_STAGGERED=ON -D QUDA_DIRAC_TWISTED_CLOVER=ON -D QUDA_DIRAC_TWISTED_MASS=ON -D QUDA_DIRAC_WILSON=ON -D QUDA_FORCE_GAUGE=ON -D QUDA_FORCE_HISQ=ON -D QUDA_GPU_ARCH={} -D QUDA_INTERFACE_MILC=ON -D QUDA_INTERFACE_QDP=ON -D QUDA_LINK_HISQ=ON -D QUDA_MPI=ON /var/tmp/quda && 
+    cmake --build . --target all -- -j$(nproc) && 
+    cd /usr/local/quda && cp -a /var/tmp/quda/build/* /usr/local/quda && 
+    echo "/usr/local/quda/lib" >> /etc/ld.so.conf.d/hpccm.conf && ldconfig && rm -rf /var/tmp/quda
+'''.format(gpu_arch)
+
 # Clonacion del repositorio QUDA
-Stage0 += shell(commands=[
-    'mkdir -p /var/tmp && cd /var/tmp && git clone --depth=1 --branch develop https://github.com/lattice/quda.git quda && cd - && \
-     cd /var/tmp/quda && \
-     mkdir -p /usr/local/quda && \
-     mkdir -p /var/tmp/quda/build && cd /var/tmp/quda/build && \
-     cmake -DCMAKE_INSTALL_PREFIX=/usr/local/quda -D CMAKE_BUILD_TYPE=RELEASE -D QUDA_DIRAC_CLOVER=ON -D QUDA_DIRAC_DOMAIN_WALL=ON -D QUDA_DIRAC_STAGGERED=ON -D QUDA_DIRAC_TWISTED_CLOVER=ON -D QUDA_DIRAC_TWISTED_MASS=ON -D QUDA_DIRAC_WILSON=ON -D QUDA_FORCE_GAUGE=ON -D QUDA_FORCE_HISQ=ON -D QUDA_GPU_ARCH={} -D QUDA_INTERFACE_MILC=ON -D QUDA_INTERFACE_QDP=ON -D QUDA_LINK_HISQ=ON -D QUDA_MPI=ON /var/tmp/quda && \
-     cmake --build . --target all -- -j$(nproc) && \
-     cd /usr/local/quda && cp -a /var/tmp/quda/build/* /usr/local/quda && \
-     echo "/usr/local/quda/lib" >> /etc/ld.so.conf.d/hpccm.conf && ldconfig && rm -rf /var/tmp/quda'.format(gpu_arch)
-])
+Stage0 += shell(commands=[quda_commands])
 
 # Construccion de MILC
 Stage0 += generic_build(branch='develop',
